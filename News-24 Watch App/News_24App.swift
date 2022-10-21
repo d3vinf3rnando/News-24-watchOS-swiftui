cdb3cf600e595758d4d6231c5d3e2ccdaebb011f
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-10-21 14:25:48 +0530

    Initial Commit

---
+++ News-24 Watch App/News_24App.swift
@@ -0,0 +1,17 @@
+//
+//  News_24App.swift
+//  News-24 Watch App
+//
+//  Created by Devin Fernando on 2022-10-21.
+//
+
+import SwiftUI
+
+@main
+struct News_24_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
