6c42007220cc1589f26e194811373874244a7867
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-10-22 18:57:58 +0530

    News pages implementation

--- News-24 Watch App/ContentView.swift
+++ News-24 Watch App/ContentView.swift
@@ -10,14 +10,133 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("News-24iii")
-               
+        ScrollView{
+            VStack {
+                Image("newsimg")
+                    .resizable()
+                    .scaledToFit()
+
+                    .frame(width: 210, height:120)
+                
+               Text("Analyst explains what Putin's latest move means")
+                    .padding(10)
+                    .fontWeight(Font.Weight.semibold)
+                Spacer()
+                    .frame(height:10)
+                   
+            }
+            
+            
+            
+            VStack {
+                Image("newsimg2")
+                    .resizable()
+                    .scaledToFit()
+
+                    .frame(width: 210, height:120)
+                
+               Text("Who is Giorgia Meloni?The rise to power of Italy's new far-right PM")
+                    .padding(10)
+                    .fontWeight(Font.Weight.semibold)
+                Spacer()
+                    .frame(height:10)
+                   
+            }
+            
+            
+            VStack {
+                Image("newsimg3")
+                    .resizable()
+                    .scaledToFit()
+
+                    .frame(width: 210, height:120)
+                
+               Text("Pound sinks as UK economic uncertainty rises")
+                    .padding(10)
+                    .fontWeight(Font.Weight.semibold)
+                Spacer()
+                    .frame(height:10)
+                   
+            }
+            
+            VStack {
+                Image("newsimg4")
+                    .resizable()
+                    .scaledToFit()
+
+                    .frame(width: 210, height:120)
+                
+               Text("Why Dijon disappeared from France")
+                    .padding(10)
+                    .fontWeight(Font.Weight.semibold)
+                Spacer()
+                    .frame(height:10)
+                   
+            }
+            
+            VStack {
+                Image("newsimg5")
+                    .resizable()
+                    .scaledToFit()
+
+                    .frame(width: 210, height:120)
+                
+               Text("New Zealand post huge 200-3 against Australia")
+                    .padding(10)
+                    .fontWeight(Font.Weight.semibold)
+                Spacer()
+                    .frame(height:10)
+                   
+            }
+            
+            VStack {
+                Image("newsimg")
+                    .resizable()
+                    .scaledToFit()
+
+                    .frame(width: 210, height:120)
+                
+               Text("Analyst explains what Putin's latest move means")
+                    .padding(10)
+                    .fontWeight(Font.Weight.semibold)
+                Spacer()
+                    .frame(height:10)
+                   
+            }
+            
+            VStack {
+                Image("newsimg")
+                    .resizable()
+                    .scaledToFit()
+
+                    .frame(width: 210, height:120)
+                
+               Text("Analyst explains what Putin's latest move means")
+                    .padding(10)
+                    .fontWeight(Font.Weight.semibold)
+                Spacer()
+                    .frame(height:10)
+                   
+            }
+            
+            VStack {
+                Image("newsimg")
+                    .resizable()
+                    .scaledToFit()
+
+                    .frame(width: 210, height:120)
+                
+               Text("Analyst explains what Putin's latest move means")
+                    .padding(10)
+                    .fontWeight(Font.Weight.semibold)
+                Spacer()
+                    .frame(height:10)
+                   
+            }
         }
-        .navigationTitle("tittle")
+        
+        
+        
         
         
         
@@ -31,3 +150,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
