f1a040bf237111dbc076753981a30b440e0494f1
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-10-23 14:25:44 +0530

    final main updates

--- News-24 Watch App/ContentView.swift
+++ News-24 Watch App/ContentView.swift
@@ -10,128 +10,142 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView{
-            VStack {
-                Image("newsimg")
-                    .resizable()
-                    .scaledToFit()
+        NavigationView{
+            ScrollView{
+                    VStack {
+                        Image("newsimg")
+                            .resizable()
+                            .scaledToFit()
 
-                    .frame(width: 210, height:120)
-                
-               Text("Analyst explains what Putin's latest move means")
-                    .padding(10)
-                    .fontWeight(Font.Weight.semibold)
-                Spacer()
-                    .frame(height:10)
-                   
-            }
-            
-            
-            
-            VStack {
-                Image("newsimg2")
-                    .resizable()
-                    .scaledToFit()
+                            .frame(width: 210, height:120)
+                        
+                        NavigationLink(destination: News1()) {
+                                        Text("Analyst explains what Putin's latest move means")
+                            
+                                .multilineTextAlignment(.leading)
+                                .foregroundColor(Color.white)
+                                
+                                .fontWeight(Font.Weight.semibold)
+                                    }
+                        .buttonStyle(.plain)
+                        .padding(10)
+                        Spacer()
+                            .frame(height: 10)
+                    
+                           
+                    }
+                    
+                    
+                    
+                    VStack {
+                        Image("newsimg2")
+                            .resizable()
+                            .scaledToFit()
 
-                    .frame(width: 210, height:120)
-                
-               Text("Who is Giorgia Meloni?The rise to power of Italy's new far-right PM")
-                    .padding(10)
-                    .fontWeight(Font.Weight.semibold)
-                Spacer()
-                    .frame(height:10)
-                   
-            }
-            
-            
-            VStack {
-                Image("newsimg3")
-                    .resizable()
-                    .scaledToFit()
+                            .frame(width: 210, height:120)
+                        
+                       Text("Who is Giorgia Meloni?The rise to power of Italy's new far-right PM")
+                            .padding(10)
+                            .fontWeight(Font.Weight.semibold)
+                        Spacer()
+                            .frame(height:10)
+                           
+                    }
+                    
+                    
+                    VStack {
+                        Image("newsimg3")
+                            .resizable()
+                            .scaledToFit()
 
-                    .frame(width: 210, height:120)
-                
-               Text("Pound sinks as UK economic uncertainty rises")
-                    .padding(10)
-                    .fontWeight(Font.Weight.semibold)
-                Spacer()
-                    .frame(height:10)
-                   
-            }
-            
-            VStack {
-                Image("newsimg4")
-                    .resizable()
-                    .scaledToFit()
+                            .frame(width: 210, height:120)
+                        
+                       Text("Pound sinks as UK economic uncertainty rises")
+                            .padding(10)
+                            .fontWeight(Font.Weight.semibold)
+                        Spacer()
+                            .frame(height:10)
+                           
+                    }
+                    
+                    VStack {
+                        Image("newsimg4")
+                            .resizable()
+                            .scaledToFit()
 
-                    .frame(width: 210, height:120)
-                
-               Text("Why Dijon disappeared from France")
-                    .padding(10)
-                    .fontWeight(Font.Weight.semibold)
-                Spacer()
-                    .frame(height:10)
-                   
-            }
-            
-            VStack {
-                Image("newsimg5")
-                    .resizable()
-                    .scaledToFit()
+                            .frame(width: 210, height:120)
+                        
+                       Text("Why Dijon disappeared from France")
+                            .padding(10)
+                            .fontWeight(Font.Weight.semibold)
+                        Spacer()
+                            .frame(height:10)
+                           
+                    }
+                    
+                    VStack {
+                        Image("newsimg5")
+                            .resizable()
+                            .scaledToFit()
 
-                    .frame(width: 210, height:120)
-                
-               Text("New Zealand post huge 200-3 against Australia")
-                    .padding(10)
-                    .fontWeight(Font.Weight.semibold)
-                Spacer()
-                    .frame(height:10)
-                   
-            }
-            
-            VStack {
-                Image("newsimg")
-                    .resizable()
-                    .scaledToFit()
+                            .frame(width: 210, height:120)
+                        
+                       Text("New Zealand post huge 200-3 against Australia")
+                            .padding(10)
+                            .fontWeight(Font.Weight.semibold)
+                        Spacer()
+                            .frame(height:10)
+                           
+                    }
+                    
+                    VStack {
+                        Image("newsimg")
+                            .resizable()
+                            .scaledToFit()
 
-                    .frame(width: 210, height:120)
-                
-               Text("Analyst explains what Putin's latest move means")
-                    .padding(10)
-                    .fontWeight(Font.Weight.semibold)
-                Spacer()
-                    .frame(height:10)
-                   
-            }
-            
-            VStack {
-                Image("newsimg")
-                    .resizable()
-                    .scaledToFit()
+                            .frame(width: 210, height:120)
+                        
+                       Text("Analyst explains what Putin's latest move means")
+                            .padding(10)
+                            .fontWeight(Font.Weight.semibold)
+                        Spacer()
+                            .frame(height:10)
+                           
+                    }
+                    
+                    VStack {
+                        Image("newsimg")
+                            .resizable()
+                            .scaledToFit()
 
-                    .frame(width: 210, height:120)
-                
-               Text("Analyst explains what Putin's latest move means")
-                    .padding(10)
-                    .fontWeight(Font.Weight.semibold)
-                Spacer()
-                    .frame(height:10)
-                   
-            }
-            
-            VStack {
-                Image("newsimg")
-                    .resizable()
-                    .scaledToFit()
+                            .frame(width: 210, height:120)
+                        
+                       Text("Analyst explains what Putin's latest move means")
+                            .padding(10)
+                            .fontWeight(Font.Weight.semibold)
+                        Spacer()
+                            .frame(height:10)
+                           
+                    }
+                    
+                    VStack {
+                        Image("newsimg")
+                            .resizable()
+                            .scaledToFit()
 
-                    .frame(width: 210, height:120)
-                
-               Text("Analyst explains what Putin's latest move means")
-                    .padding(10)
-                    .fontWeight(Font.Weight.semibold)
-                Spacer()
-                    .frame(height:10)
-                   
+                            .frame(width: 210, height:120)
+                        
+                       Text("Analyst explains what Putin's latest move means")
+                            .padding(10)
+                            .fontWeight(Font.Weight.semibold)
+                        Spacer()
+                            .frame(height:10)
+                           
+                    }
+                  
+                    
+                }
+            .navigationTitle("News 24")
             }
         }
         
@@ -143,7 +157,7 @@ struct ContentView: View {
         
     }
     
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -151,4 +165,15 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-
+struct News1:View{
+    var body: some View{
+        ScrollView{
+            VStack{
+                Text("Russian President Vladimir Putin announced that he has signed a law introducing martial law in four Ukrainian regions the Kremlin claims to have annexed, in violation of international law. CNN political and national security analyst David Sanger explains.")
+                    .padding(10)
+            }
+        }
+        .navigationTitle("News 24")
+    }
+        
+}
