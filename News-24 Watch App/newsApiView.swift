57c1759ee42ebc385be6cd815fb2467ca902de85
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-10-28 12:04:33 +0530

    api view

---
+++ News-24 Watch App/newsApiView.swift
@@ -0,0 +1,20 @@
+//
+//  newsApiView.swift
+//  News-24 Watch App
+//
+//  Created by Devin Fernando on 2022-10-27.
+//
+
+import SwiftUI
+
+struct newsApiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct newsApiView_Previews: PreviewProvider {
+    static var previews: some View {
+        newsApiView()
+    }
+}
